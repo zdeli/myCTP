4533d6465000045e6925f6dcca45c973f1c73474
williamlfang <william.lian.fang@gmail.com>
2018-06-07 10:48:56 +0800

    自动清理文件

---
+++ vnpy-1.7.2/vnpy/trader/clean.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## 清理原来的 .c .so 文件
+
+## =============================================================================
+# rm -f ./*.c
+rm -f ./*.so
+
+# rm -f ./app/ctaStrategy/*.c
+rm -f ./app/ctaStrategy/*.so
+
+# rm -f ./app/ctaStrategy/*.c
+rm -f ./app/ctaStrategy/strategy/*.so
+
+# rm -f ./app/dataRecorder/*.c
+rm -f ./app/dataRecorder/*.so
+
+# rm -f ./app/riskManager/*.c
+rm -f ./app/riskManager/*.so
+
+# rm -f ./gateway/ctpGateway/*.c
+rm -f ./gateway/ctpGateway/*.so
+
+# rm -f ./gateway/ctpGatewayRecorder/*.c
+rm -f ./gateway/ctpGatewayRecorder/*.so
+## =============================================================================
+
+
+## =============================================================================
+python setup.py build_ext --inplace
+## =============================================================================
