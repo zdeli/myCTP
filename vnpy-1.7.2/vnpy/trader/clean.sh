d797ac5a4e6b87165e7bc1d0459313c7d25111ec
williamlfang <william.lian.fang@gmail.com>
2018-06-19 13:44:18 +0800

    编译后删除不惜要的文件

--- vnpy-1.7.2/vnpy/trader/clean.sh
+++ vnpy-1.7.2/vnpy/trader/clean.sh
@@ -36,3 +36,27 @@ rm -rf ./app/**/build
 rm -rf ./gateway/**/build
 rm -rf ../event/**/build
 ## =============================================================================
+
+
+## =============================================================================
+rm -f ./*.c
+rm -f ./*.so
+
+rm -f ./app/ctaStrategy/*.c
+rm -f ./app/ctaStrategy/*.so
+
+rm -f ./app/ctaStrategy/strategy/*.c
+rm -f ./app/ctaStrategy/strategy/*.so
+
+rm -f ./app/dataRecorder/*.c
+rm -f ./app/dataRecorder/*.so
+
+rm -f ./app/riskManager/*.c
+rm -f ./app/riskManager/*.so
+
+rm -f ./gateway/ctpGateway/*.c
+rm -f ./gateway/ctpGateway/*.so
+
+rm -f ./gateway/ctpGatewayRecorder/*.c
+rm -f ./gateway/ctpGatewayRecorder/*.so
+## =============================================================================
