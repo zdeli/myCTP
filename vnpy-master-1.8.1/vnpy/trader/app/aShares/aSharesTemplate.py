3512b28b593a69052fe99c8826d69468fc6c9cfd
williamlfang <william.lian.fang@gmail.com>
2018-06-29 11:16:54 +0800

    已经可以实现基本的下单功能

---
+++ vnpy-master-1.8.1/vnpy/trader/app/aShares/aSharesTemplate.py
@@ -0,0 +1,475 @@
+# encoding: UTF-8
+
+'''
+本文件包含了 ASHARES 引擎中的策略开发用模板，开发策略时需要继承 ASharesTemplate类。
+'''
+
+import numpy as np
+import talib
+
+from vnpy.trader.vtConstant import *
+from vnpy.trader.vtObject import VtBarData
+
+from .aSharesBase import *
+
+########################################################################
+class ASharesTemplate(object):
+    """CTA策略模板"""
+    
+    # 策略类的名称和作者
+    className = 'ASharesTemplate'
+    author = EMPTY_UNICODE
+    
+    # MongoDB数据库的名称，K线数据库默认为1分钟
+    # tickDbName = TICK_DB_NAME
+    # barDbName = MINUTE_DB_NAME
+    
+    # 策略的基本参数
+    name = EMPTY_UNICODE           # 策略实例名称
+    vtSymbol = EMPTY_STRING        # 交易的合约vt系统代码    
+    productClass = EMPTY_STRING    # 产品类型（只有IB接口需要）
+    currency = EMPTY_STRING        # 货币（只有IB接口需要）
+    
+    # 策略的基本变量，由引擎管理
+    inited = False                 # 是否进行了初始化
+    trading = False                # 是否启动交易，由引擎管理
+    pos = 0                        # 持仓情况
+    
+    # 参数列表，保存了参数的名称
+    paramList = ['name',
+                 'className',
+                 'author',
+                 'vtSymbol']
+    
+    # 变量列表，保存了变量的名称
+    varList = ['inited',
+               'trading',
+               'pos']
+    
+    # 同步列表，保存了需要保存到数据库的变量名称
+    syncList = ['pos']
+
+    #----------------------------------------------------------------------
+    def __init__(self, aSharesEngine, setting):
+        """Constructor"""
+        self.aSharesEngine = aSharesEngine
+
+        # 设置策略的参数
+        if setting:
+            d = self.__dict__
+            for key in self.paramList:
+                if key in setting:
+                    d[key] = setting[key]
+    
+    #----------------------------------------------------------------------
+    def onInit(self):
+        """初始化策略（必须由用户继承实现）"""
+        raise NotImplementedError
+    
+    #----------------------------------------------------------------------
+    def onStart(self):
+        """启动策略（必须由用户继承实现）"""
+        raise NotImplementedError
+    
+    #----------------------------------------------------------------------
+    def onStop(self):
+        """停止策略（必须由用户继承实现）"""
+        raise NotImplementedError
+
+    #----------------------------------------------------------------------
+    def onTick(self, tick):
+        """收到行情TICK推送（必须由用户继承实现）"""
+        raise NotImplementedError
+
+    #----------------------------------------------------------------------
+    def onOrder(self, order):
+        """收到委托变化推送（必须由用户继承实现）"""
+        raise NotImplementedError
+    
+    #----------------------------------------------------------------------
+    def onTrade(self, trade):
+        """收到成交推送（必须由用户继承实现）"""
+        raise NotImplementedError
+    
+    #----------------------------------------------------------------------
+    def onBar(self, bar):
+        """收到Bar推送（必须由用户继承实现）"""
+        raise NotImplementedError
+    
+    #----------------------------------------------------------------------
+    def onStopOrder(self, so):
+        """收到停止单推送（必须由用户继承实现）"""
+        raise NotImplementedError
+    
+    #----------------------------------------------------------------------
+    def buy(self, vtSymbol, price, volume):
+        """股票买入"""
+        return self.sendOrder(vtSymbol, ASHARES_BUY, price, volume)
+    
+    #----------------------------------------------------------------------
+    def sell(self, vtSymbol, price, volume):
+        """股票卖出"""
+        return self.sendOrder(vtSymbol, ASHARES_SELL, price, volume)       
+        
+    #----------------------------------------------------------------------
+    def sendOrder(self, vtSymbol, orderType, price, volume):
+        """发送委托"""
+        if self.trading:
+            return self.aSharesEngine.sendOrder(vtSymbol, orderType, price, volume, self)
+        else:
+            # 交易停止时发单返回空字符串
+            self.writeASharesLog(u'交易状态已停止')
+            return []
+        
+    #----------------------------------------------------------------------
+    def cancelOrder(self, vtOrderID):
+        """撤单"""
+        # 如果发单号为空字符串，则不进行后续操作
+        if not vtOrderID:
+            return
+        self.aSharesEngine.cancelOrder(vtOrderID)
+
+    #----------------------------------------------------------------------
+    def cancelAll(self):
+        """全部撤单"""
+        self.aSharesEngine.cancelAll(self.name)
+    
+    #----------------------------------------------------------------------
+    def loadTick(self, days):
+        """读取tick数据"""
+        # return self.aSharesEngine.loadTick(self.tickDbName, self.vtSymbol, days)
+        pass
+
+    #----------------------------------------------------------------------
+    def loadBar(self, days):
+        """读取bar数据"""
+        # return self.aSharesEngine.loadBar(self.barDbName, self.vtSymbol, days)
+        pass
+
+    #----------------------------------------------------------------------
+    def writeASharesLog(self, content):
+        """记录CTA日志"""
+        content = self.name + ':' + content
+        self.aSharesEngine.writeASharesLog(content)
+        
+    #----------------------------------------------------------------------
+    def putEvent(self):
+        """发出策略状态变化事件"""
+        self.aSharesEngine.putStrategyEvent(self.name)
+        
+    #----------------------------------------------------------------------
+    def getEngineType(self):
+        """查询当前运行的环境"""
+        return self.aSharesEngine.engineType
+    
+    #----------------------------------------------------------------------
+    def saveSyncData(self):
+        """保存同步数据到数据库"""
+        if self.trading:
+            self.aSharesEngine.saveSyncData(self)
+    
+    #----------------------------------------------------------------------
+    def getPriceTick(self, vtSymbol):
+        """查询最小价格变动"""
+        return self.aSharesEngine.getPriceTick(vtSymbol)
+        
+
+########################################################################
+class BarGenerator(object):
+    """
+    K线合成器，支持：
+    1. 基于Tick合成1分钟K线
+    2. 基于1分钟K线合成X分钟K线（X可以是2、3、5、10、15、30	）
+    """
+
+    #----------------------------------------------------------------------
+    def __init__(self, onBar, xmin=0, onXminBar=None):
+        """Constructor"""
+        self.bar = None             # 1分钟K线对象
+        self.onBar = onBar          # 1分钟K线回调函数
+        
+        self.xminBar = None         # X分钟K线对象
+        self.xmin = xmin            # X的值
+        self.onXminBar = onXminBar  # X分钟K线的回调函数
+        
+        self.lastTick = None        # 上一TICK缓存对象
+        
+    #----------------------------------------------------------------------
+    def updateTick(self, tick):
+        """TICK更新"""
+        newMinute = False   # 默认不是新的一分钟
+        
+        # 尚未创建对象
+        if not self.bar:
+            self.bar = VtBarData()
+            newMinute = True
+        # 新的一分钟
+        elif self.bar.datetime.minute != tick.datetime.minute:
+            # 生成上一分钟K线的时间戳
+            self.bar.datetime = self.bar.datetime.replace(second=0, microsecond=0)  # 将秒和微秒设为0
+            self.bar.date = self.bar.datetime.strftime('%Y%m%d')
+            self.bar.time = self.bar.datetime.strftime('%H:%M:%S.%f')
+            
+            # 推送已经结束的上一分钟K线
+            self.onBar(self.bar)
+            
+            # 创建新的K线对象
+            self.bar = VtBarData()
+            newMinute = True
+            
+        # 初始化新一分钟的K线数据
+        if newMinute:
+            self.bar.vtSymbol = tick.vtSymbol
+            self.bar.symbol = tick.symbol
+            self.bar.exchange = tick.exchange
+
+            self.bar.open = tick.lastPrice
+            self.bar.high = tick.lastPrice
+            self.bar.low = tick.lastPrice
+        # 累加更新老一分钟的K线数据
+        else:                                   
+            self.bar.high = max(self.bar.high, tick.lastPrice)
+            self.bar.low = min(self.bar.low, tick.lastPrice)
+
+        # 通用更新部分
+        self.bar.close = tick.lastPrice        
+        self.bar.datetime = tick.datetime  
+        self.bar.openInterest = tick.openInterest
+   
+        if self.lastTick:
+            volumeChange = tick.volume - self.lastTick.volume   # 当前K线内的成交量
+            self.bar.volume += max(volumeChange, 0)             # 避免夜盘开盘lastTick.volume为昨日收盘数据，导致成交量变化为负的情况
+            
+        # 缓存Tick
+        self.lastTick = tick
+
+    #----------------------------------------------------------------------
+    def updateBar(self, bar):
+        """1分钟K线更新"""
+        # 尚未创建对象
+        if not self.xminBar:
+            self.xminBar = VtBarData()
+            
+            self.xminBar.vtSymbol = bar.vtSymbol
+            self.xminBar.symbol = bar.symbol
+            self.xminBar.exchange = bar.exchange
+        
+            self.xminBar.open = bar.open
+            self.xminBar.high = bar.high
+            self.xminBar.low = bar.low            
+            
+            self.xminBar.datetime = bar.datetime    # 以第一根分钟K线的开始时间戳作为X分钟线的时间戳
+        # 累加老K线
+        else:
+            self.xminBar.high = max(self.xminBar.high, bar.high)
+            self.xminBar.low = min(self.xminBar.low, bar.low)
+    
+        # 通用部分
+        self.xminBar.close = bar.close        
+        self.xminBar.openInterest = bar.openInterest
+        self.xminBar.volume += int(bar.volume)                
+            
+        # X分钟已经走完
+        if not (bar.datetime.minute + 1) % self.xmin:   # 可以用X整除
+            # 生成上一X分钟K线的时间戳
+            self.xminBar.datetime = self.xminBar.datetime.replace(second=0, microsecond=0)  # 将秒和微秒设为0
+            self.xminBar.date = self.xminBar.datetime.strftime('%Y%m%d')
+            self.xminBar.time = self.xminBar.datetime.strftime('%H:%M:%S.%f')
+            
+            # 推送
+            self.onXminBar(self.xminBar)
+            
+            # 清空老K线缓存对象
+            self.xminBar = None
+
+
+########################################################################
+class ArrayManager(object):
+    """
+    K线序列管理工具，负责：
+    1. K线时间序列的维护
+    2. 常用技术指标的计算
+    """
+
+    #----------------------------------------------------------------------
+    def __init__(self, size=100):
+        """Constructor"""
+        self.count = 0                      # 缓存计数
+        self.size = size                    # 缓存大小
+        self.inited = False                 # True if count>=size
+        
+        self.openArray = np.zeros(size)     # OHLC
+        self.highArray = np.zeros(size)
+        self.lowArray = np.zeros(size)
+        self.closeArray = np.zeros(size)
+        self.volumeArray = np.zeros(size)
+        
+    #----------------------------------------------------------------------
+    def updateBar(self, bar):
+        """更新K线"""
+        self.count += 1
+        if not self.inited and self.count >= self.size:
+            self.inited = True
+        
+        self.openArray[0:self.size-1] = self.openArray[1:self.size]
+        self.highArray[0:self.size-1] = self.highArray[1:self.size]
+        self.lowArray[0:self.size-1] = self.lowArray[1:self.size]
+        self.closeArray[0:self.size-1] = self.closeArray[1:self.size]
+        self.volumeArray[0:self.size-1] = self.volumeArray[1:self.size]
+    
+        self.openArray[-1] = bar.open
+        self.highArray[-1] = bar.high
+        self.lowArray[-1] = bar.low        
+        self.closeArray[-1] = bar.close
+        self.volumeArray[-1] = bar.volume
+        
+    #----------------------------------------------------------------------
+    @property
+    def open(self):
+        """获取开盘价序列"""
+        return self.openArray
+        
+    #----------------------------------------------------------------------
+    @property
+    def high(self):
+        """获取最高价序列"""
+        return self.highArray
+    
+    #----------------------------------------------------------------------
+    @property
+    def low(self):
+        """获取最低价序列"""
+        return self.lowArray
+    
+    #----------------------------------------------------------------------
+    @property
+    def close(self):
+        """获取收盘价序列"""
+        return self.closeArray
+    
+    #----------------------------------------------------------------------
+    @property    
+    def volume(self):
+        """获取成交量序列"""
+        return self.volumeArray
+    
+    #----------------------------------------------------------------------
+    def sma(self, n, array=False):
+        """简单均线"""
+        result = talib.SMA(self.close, n)
+        if array:
+            return result
+        return result[-1]
+        
+    #----------------------------------------------------------------------
+    def std(self, n, array=False):
+        """标准差"""
+        result = talib.STDDEV(self.close, n)
+        if array:
+            return result
+        return result[-1]
+    
+    #----------------------------------------------------------------------
+    def cci(self, n, array=False):
+        """CCI指标"""
+        result = talib.CCI(self.high, self.low, self.close, n)
+        if array:
+            return result
+        return result[-1]
+        
+    #----------------------------------------------------------------------
+    def atr(self, n, array=False):
+        """ATR指标"""
+        result = talib.ATR(self.high, self.low, self.close, n)
+        if array:
+            return result
+        return result[-1]
+        
+    #----------------------------------------------------------------------
+    def rsi(self, n, array=False):
+        """RSI指标"""
+        result = talib.RSI(self.close, n)
+        if array:
+            return result
+        return result[-1]
+    
+    #----------------------------------------------------------------------
+    def macd(self, fastPeriod, slowPeriod, signalPeriod, array=False):
+        """MACD指标"""
+        macd, signal, hist = talib.MACD(self.close, fastPeriod,
+                                        slowPeriod, signalPeriod)
+        if array:
+            return macd, signal, hist
+        return macd[-1], signal[-1], hist[-1]
+    
+    #----------------------------------------------------------------------
+    def adx(self, n, array=False):
+        """ADX指标"""
+        result = talib.ADX(self.high, self.low, self.close, n)
+        if array:
+            return result
+        return result[-1]
+    
+    #----------------------------------------------------------------------
+    def boll(self, n, dev, array=False):
+        """布林通道"""
+        mid = self.sma(n, array)
+        std = self.std(n, array)
+        
+        up = mid + std * dev
+        down = mid - std * dev
+        
+        return up, down    
+    
+    #----------------------------------------------------------------------
+    def keltner(self, n, dev, array=False):
+        """肯特纳通道"""
+        mid = self.sma(n, array)
+        atr = self.atr(n, array)
+        
+        up = mid + atr * dev
+        down = mid - atr * dev
+        
+        return up, down
+    
+    #----------------------------------------------------------------------
+    def donchian(self, n, array=False):
+        """唐奇安通道"""
+        up = talib.MAX(self.high, n)
+        down = talib.MIN(self.low, n)
+        
+        if array:
+            return up, down
+        return up[-1], down[-1]
+    
+
+# ########################################################################
+# class CtaSignal(object):
+#     """
+#     CTA策略信号，负责纯粹的信号生成（目标仓位），不参与具体交易管理
+#     """
+
+#     #----------------------------------------------------------------------
+#     def __init__(self):
+#         """Constructor"""
+#         self.signalPos = 0      # 信号仓位
+    
+#     #----------------------------------------------------------------------
+#     def onBar(self, bar):
+#         """K线推送"""
+#         pass
+    
+#     #----------------------------------------------------------------------
+#     def onTick(self, tick):
+#         """Tick推送"""
+#         pass
+        
+#     #----------------------------------------------------------------------
+#     def setSignalPos(self, pos):
+#         """设置信号仓位"""
+#         self.signalPos = pos
+        
+#     #----------------------------------------------------------------------
+#     def getSignalPos(self):
+#         """获取信号仓位"""
+#         return self.signalPos
